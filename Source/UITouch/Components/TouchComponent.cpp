8e911701bbe31f7b7cc09545b1340e2f9e3b941b
Aimo <56662851+AimoTvT@users.noreply.github.com>
2022-10-05 14:36:28 +0800

    初始化
    最新的记录文件

--- Source/UITouch/Components/TouchComponent.cpp
+++ Source/UITouch/Components/TouchComponent.cpp
@@ -1,5 +1,15 @@
 /**
+ * Copyright: Aimo_皑墨
+ * Release date: October 10, 2022
+ * Do not steal or use it for profit
+ * No modification is allowed to be published on the Internet without the person's consent
+ * BiLiBiLi (哔哩哔哩) address: https://space.bilibili.com/146962867
+ * making address: https://github.com/AimoTvT/UETouchPlugin
+ * We welcome the contributions of powerful movers and movers to join this plugin
+ * Build powerful plugins together!!
+ *
  * 版权所有权: Aimo_皑墨
+ * 发布时间: 2022年10月10号
  * 禁止盗取或用于盈利
  * 禁止修改未经过本人同意发布互联网
  * BiLiBiLi(哔哩哔哩)地址: https://space.bilibili.com/146962867

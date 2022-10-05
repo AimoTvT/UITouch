16ef6275521eded21c8f010b889008de8265e7b3
Aimo <56662851+AimoTvT@users.noreply.github.com>
2022-10-05 14:32:00 +0800

    初始化
    修改URL
    添加所有权...

--- Source/UITouch/Widgets/TouchWidget.h
+++ Source/UITouch/Widgets/TouchWidget.h
@@ -1,12 +1,22 @@
 /**
+ * Copyright: Aimo_皑墨
+ * Release date: October 10, 2022
+ * Do not steal or use it for profit
+ * No modification is allowed to be published on the Internet without the person's consent
+ * BiLiBiLi (哔哩哔哩) address: https://space.bilibili.com/146962867
+ * making address: https://github.com/AimoTvT/UETouchPlugin
+ * We welcome the contributions of powerful movers and movers to join this plugin
+ * Build powerful plugins together!!
+ *
  * 版权所有权: Aimo_皑墨
+ * 发布时间: 2022年10月10号
  * 禁止盗取或用于盈利
  * 禁止修改未经过本人同意发布互联网
  * BiLiBiLi(哔哩哔哩)地址: https://space.bilibili.com/146962867
  * GitHub地址: https://github.com/AimoTvT/UETouchPlugin
  * 欢迎有实力的大佬/萌新加入本插件的贡献
  * 一起打造强大的插件!!!
- */ 
+ */
 
 #pragma once
 

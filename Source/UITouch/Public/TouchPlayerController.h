2a9b713ffd2c6ef56bd17dfb04ad07324e6cea49
Aimo <56662851+AimoTvT@users.noreply.github.com>
2022-10-11 17:03:36 +0800

    初始化
    初始化

--- Source/UITouch/Public/TouchPlayerController.h
+++ Source/UITouch/Public/TouchPlayerController.h
@@ -1,6 +1,6 @@
 /**
  * Copyright: Aimo_皑墨
- * Release date: October 10, 2022
+ * Release date: October 12, 2022
  * Do not steal or use it for profit
  * No modification is allowed to be published on the Internet without the person's consent
  * BiLiBiLi (哔哩哔哩) address: https://space.bilibili.com/146962867
@@ -9,7 +9,7 @@
  * Build powerful plugins together!!
  *
  * 版权所有权: Aimo_皑墨
- * 发布时间: 2022年10月10号
+ * 发布时间: 2022年10月12号
  * 禁止盗取或用于盈利
  * 禁止修改未经过本人同意发布互联网
  * BiLiBiLi(哔哩哔哩)地址: https://space.bilibili.com/146962867
@@ -54,15 +54,15 @@ protected:
 public:
 
 	UFUNCTION(BlueprintCallable, Category = "Aimo|Function")
-		/** * 设置操控杆位置 */
+		/** * 触摸按下 */
 		virtual void TouchPressed(ETouchIndex::Type FingerIndex, FVector Location);
 
 	UFUNCTION(BlueprintCallable, Category = "Aimo|Function")
-		/** * 设置操控杆位置 */
+		/** * 触摸松开 */
 		virtual void TouchReleased(ETouchIndex::Type FingerIndex, FVector Location);
 
 	UFUNCTION(BlueprintCallable, Category = "Aimo|Function")
-		/** * 设置操控杆位置 */
+		/** * 触摸移动 */
 		virtual void TouchMove(ETouchIndex::Type FingerIndex, FVector Location);
 
 };

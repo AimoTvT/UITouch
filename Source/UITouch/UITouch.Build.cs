1d0158b678db6b70cf1f7797bb50f46532f28a2a
Aimo <56662851+AimoTvT@users.noreply.github.com>
2022-10-27 13:46:09 +0800

    修改版权注释,平台白名单,添加Logo
    修改版权注释,平台白名单,添加Logo

--- Source/UITouch/UITouch.Build.cs
+++ Source/UITouch/UITouch.Build.cs
@@ -1,19 +1,19 @@
 /**
  * Copyright: Aimo_皑墨
- * Release date: October 12, 2022
+ * Release date: October 21, 2022
  * Do not steal or use it for profit
  * No modification is allowed to be published on the Internet without the person's consent
  * BiLiBiLi (哔哩哔哩) address: https://space.bilibili.com/146962867
- * making address: https://github.com/AimoTvT/UETouchPlugin
+ * making address: https://github.com/AimoTvT/UITouch
  * We welcome the contributions of powerful movers and movers to join this plugin
  * Build powerful plugins together!!
  * 
  * 版权所有权: Aimo_皑墨
- * 发布时间: 2022年10月12号
+ * 发布时间: 2022年10月21号
  * 禁止盗取或用于盈利
  * 禁止修改未经过本人同意发布互联网
  * BiLiBiLi(哔哩哔哩)地址: https://space.bilibili.com/146962867
- * GitHub地址: https://github.com/AimoTvT/UETouchPlugin
+ * GitHub地址: https://github.com/AimoTvT/UITouch
  * 欢迎有实力的大佬/萌新加入本插件的贡献
  * 一起打造强大的插件!!!
  */
